05f889fb63c629926d9f240b6f2b48acaa315c6a
Aya Al Sabahi <69956719+ayaalsabahi@users.noreply.github.com>
2021-09-26 00:02:52 +0300

    Update Reverse_Integer.py

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,2 +1,8 @@
 # Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0. Adapted from leetcode.
-        
+
+def reverseNum(n):
+    n = str(n)
+    reversed= n[::-1]
+    
+return int(revesed)
+    
